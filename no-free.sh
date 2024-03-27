366c5383fc1991309fa7947b425f02d25f85b560
TatoEb <109128185+TatoEb@users.noreply.github.com>
2024-03-27 12:39:43 +0200

    Update no-free.sh

--- no-free.sh
+++ no-free.sh
@@ -1,1 +1,1 @@
-cd ~ && chmod 777 -R x100-for-docker/put-your-ovpn-files-here && sudo rm -rfd your-future-vpn-1-name your-future-vpn-2-name CactusVpn FreeAndSlowVpn
+cd ~ && sudo chmod 777 -R x100-for-docker/put-your-ovpn-files-here && sudo rm -rfd your-future-vpn-1-name your-future-vpn-2-name CactusVpn FreeAndSlowVpn

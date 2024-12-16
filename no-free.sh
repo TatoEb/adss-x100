55278f01988f1f9150d1ea6c73edb1619406ed1d
TatoEb <109128185+TatoEb@users.noreply.github.com>
2024-12-16 10:22:13 +0200

    Delete no-free.sh

--- no-free.sh
+++
@@ -1,1 +0,0 @@
-cd ~ && sudo chmod 777 -R x100-for-docker/put-your-ovpn-files-here && cd x100-for-docker/put-your-ovpn-files-here && sudo rm -rfd your-future-vpn-1-name your-future-vpn-2-name CactusVpn FreeAndSlowVpn

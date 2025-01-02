f3c2a4d7cc97496873d52a10e137f6d993c05092
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-01-02 15:17:49 +0200

    Update updX100.sh

--- updX100.sh
+++ updX100.sh
@@ -1,1 +1,26 @@
-wget -q -O credentials.txt https://github.com/TatoEb/OVPN/raw/refs/heads/main/updX100.md && sudo cp -f credentials.txt x100-for-docker/put-your-ovpn-files-here/TRIAL.24hours && sudo rm -rf credentials.txt
+#!/bin/bash
+# Змінні
+URL="https://github.com/TatoEb/OVPN/raw/refs/heads/main/updX100.md"
+LOCAL_FILE="credentials.txt"
+HEADERS_FILE="checking.txt"
+cd x100-for-docker/put-your-ovpn-files-here/TRIAL.24hours
+# Завантаження заголовків
+wget -q --spider --server-response -O /dev/null $URL 2>&1 | grep -iE "Last-Modified|ETag" > $HEADERS_FILE.tmp
+
+# Перевірка наявності старих заголовків
+if [[ -f $HEADERS_FILE ]]; then
+    if ! cmp -s $HEADERS_FILE $HEADERS_FILE.tmp; then
+        #echo "Файл змінився. Завантажую..."
+        wget -q -O $LOCAL_FILE $URL
+        mv $HEADERS_FILE.tmp $HEADERS_FILE
+    else
+        #echo "Файл не змінився."
+        rm $HEADERS_FILE.tmp
+    fi
+else
+    #echo "Завантажую файл вперше..."
+    wget -q -O $LOCAL_FILE $URL
+    mv $HEADERS_FILE.tmp $HEADERS_FILE
+fi
+cd ~
+

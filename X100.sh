30ee56c8c0b08865ad35385a9a58f3a564a12ef4
TatoEb <109128185+TatoEb@users.noreply.github.com>
2025-02-25 20:31:11 +0200

    Update X100.sh

--- X100.sh
+++ X100.sh
@@ -1,1 +1,1 @@
-cd ~ && sudo chmod 777 -R x100-for-docker && sudo screen -S X100 bash x100-for-docker/for-macOS-and-Linux-hosts/run-and-auto-update.bash && cd ~
+cd ~ && sudo chmod 777 -R x100-for-docker/put-your-ovpn-files-here && cd x100-for-docker/put-your-ovpn-files-here && sudo rm -rf *log*.txt && cd ~ && sudo screen -S X100 bash x100-for-docker/for-macOS-and-Linux-hosts/run-and-auto-update.bash && cd ~
